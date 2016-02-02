52c281bf0985bb67afac2535e22ddb3fa4e09ebf
Marlan Green <marlangreen@storm.cis.fordham.edu>
2016-02-01 20:42:06 -0500

    HelloWorld!

---
+++ HelloWorld.cc
@@ -0,0 +1,15 @@
+// cin with strings
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+
+	string s;
+	cin >> s;
+	cout << "Hello" << s << endl;
+  std::cout << "Hello World!";
+  return 0;
+
+}
